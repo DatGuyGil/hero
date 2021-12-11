fff1cc1f514f097241d37e7194e8eacd447bc42c
up201604138 <up201604138@fe.up.pt>
2021-12-11 10:28:25 +0000

    Added hero files

---
+++ src/main/java/Coin.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+public class Coin {
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        for (int i = 0; i < 5; i++)
+            coins.add(new Coin(random.nextInt(width - 2) +
+                    1, random.nextInt(height - 2) + 1));
+        return coins;
+    }
+}
+
+*/
